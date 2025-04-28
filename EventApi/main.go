6e146178ef0cc37c0e0afeba6543ff9fc00810bb
Manjodh Singh <manjodhsaran1@gmail.com>
2025-04-28 09:48:47 +0530

    update event

--- EventApi/main.go
+++ EventApi/main.go
@@ -1,79 +1,14 @@
 package main
 
 import (
-	"net/http"
-	"strconv"
-
 	"eventapi.com/db"
-	"eventapi.com/models"
+	"eventapi.com/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	db.InitDB()
 	server := gin.Default()
-
-	server.GET("/events", getEvents)
-	server.GET("/events/:id", getEvent)
-	server.POST("/events", createEvents)
-
+	routes.RegisteredRoutes(server)
 	server.Run(":8080")
-}
-
-func getEvent(context *gin.Context) {
-	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse data"})
-		return
-	}
-
-	event, err := models.GetEvent(id)
-
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event"})
-		return
-	}
-
-	if event == nil {
-		context.JSON(http.StatusNotFound, gin.H{"message": "Event not found"})
-		return
-	}
-
-	context.JSON(http.StatusOK, event)
-}
-
-func getEvents(context *gin.Context) {
-	events, err := models.GetAllEvents()
-
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events"})
-		return
-	}
-	if len(events) == 0 {
-		context.JSON(http.StatusNotFound, gin.H{"message": "No events found"})
-		return
-	}
-
-	context.JSON(http.StatusOK, events)
-}
-
-func createEvents(context *gin.Context) {
-	var event models.Event
-	err := context.ShouldBindJSON(&event)
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse data"})
-	}
-
-	event.ID = 1
-	event.UserId = 1
-
-	err = event.Save()
-
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save event"})
-		return
-	}
-
-	context.JSON(http.StatusCreated, gin.H{"message": "Event created", "data": event})
 }
---
+++ EventApi/routes/events.go
@@ -0,0 +1,100 @@
+package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"eventapi.com/models"
+	"github.com/gin-gonic/gin"
+)
+
+func getEvent(context *gin.Context) {
+	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse data"})
+		return
+	}
+
+	event, err := models.GetEvent(id)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event"})
+		return
+	}
+
+	if event == nil {
+		context.JSON(http.StatusNotFound, gin.H{"message": "Event not found"})
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
+}
+
+func getEvents(context *gin.Context) {
+	events, err := models.GetAllEvents()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events"})
+		return
+	}
+	if len(events) == 0 {
+		context.JSON(http.StatusNotFound, gin.H{"message": "No events found"})
+		return
+	}
+
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvents(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse data"})
+	}
+
+	event.ID = 1
+	event.UserId = 1
+
+	err = event.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save event"})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created", "data": event})
+}
+
+func updateEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse data"})
+		return
+	}
+
+	_, err = models.GetEvent(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event"})
+		return
+	}
+
+	var updatedEvent models.Event
+
+	err = context.ShouldBindJSON(&updatedEvent)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse data"})
+	}
+
+	updatedEvent.ID = eventId
+	err = updatedEvent.Update()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save event"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Event created", "data": updatedEvent})
+}
